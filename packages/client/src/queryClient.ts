4c2c2e9742cb5a6b6736eca707c8e6667b5930fd
wiresnchains <dgrachov@icloud.com>
2025-04-18 10:59:29 +0200

    Add caching & update package version

--- packages/client/src/queryClient.ts
+++ packages/client/src/queryClient.ts
@@ -1,13 +1,45 @@
 import { ResolverOutput, SingularityQLStatus } from "../../shared/index";
+import stringify from "json-stable-stringify";
+
+type CacheEntry = {
+    value: ResolverOutput;
+    expiresAt: number;
+};
+
+export type QueryOptions = {
+    placeholders?: { [key: string]: any };
+    cacheTTL?: number;
+};
 
 export class QueryClient {
     private serverUrl: string;
+    private cache: Map<string, CacheEntry> = new Map();
+    private defaultTTL: number = 5 * 60 * 1000;
 
     constructor(serverUrl: string) {
         this.serverUrl = serverUrl;
     }
 
-    public async query(query: string, placeholders: { [key: string]: any }): Promise<ResolverOutput | undefined> {
+    public async query(query: string, options: QueryOptions): Promise<ResolverOutput | undefined> {
+        const placeholders = options.placeholders || {};
+
+        const key = this.createCacheKey(query, placeholders);
+        const now = Date.now();
+
+        if (key) {
+            const cached = this.cache.get(key);
+
+            if (cached) {
+                if (cached.expiresAt > now)
+                    return cached.value;
+    
+                this.cache.delete(key);
+            }
+        }
+        else {
+            console.error("Failed to generate cache key");
+        }
+
         try {
             const response = await fetch(this.serverUrl, {
                 method: "POST",
@@ -26,11 +58,22 @@ export class QueryClient {
     
             if (body.status != SingularityQLStatus.Ok)
                 throw new Error(body.error);
+
+            if (key) {
+                this.cache.set(key, {
+                    value: body,
+                    expiresAt: now + (options.cacheTTL == undefined ? this.defaultTTL : options.cacheTTL)
+                });
+            }
     
             return body;
         }
         catch (err) {
             console.error(err);
         }
+    }
+
+    private createCacheKey(query: string, placeholders: { [key: string]: any }): string | undefined {
+        return stringify({ query, placeholders });
     }
 }
